589ba5d2c46815a23c4ffdd0b171185fbcb32fe7
Rati <rati@Krishnas-iMac.local>
2017-06-15 12:10:16 +0530

    First commit

---
+++ PRLoader.swift
@@ -0,0 +1,39 @@
+//
+//  PRLoader.swift
+//
+//  Created by Ratti on 23/01/17.
+//  Copyright © 2017 Ratti. All rights reserved.
+//
+
+import UIKit
+
+class PRLoader: NSObject {
+    
+   static let view : UIView = UIView()
+   static let activity : UIActivityIndicatorView = UIActivityIndicatorView.init()
+   static let indicator : DGActivityIndicatorView = DGActivityIndicatorView.init(type: .ballClipRotate, tintColor: UIColor.blue, size: 50)
+   static func PRload()
+    {
+        
+        
+        indicator.frame = CGRect.init(x: 0, y: 0, width: 50, height: 50)
+        view.frame = UIApplication.shared.keyWindow!.rootViewController!.view.bounds
+        view.backgroundColor = UIColor.clear
+        activity.activityIndicatorViewStyle = .whiteLarge
+        activity.color = UIColor.black
+        indicator.startAnimating()
+        activity.startAnimating()
+        indicator.center = view.center
+       // indicator.type = .rotatingTrigons   //To change types of loader
+        activity.center = view.center
+        view.addSubview(indicator)
+        UIApplication.shared.keyWindow?.addSubview(view)
+    }
+    
+    static func PRUnload()
+    {
+        view.removeFromSuperview()
+        indicator.removeFromSuperview()
+    }
+
+}
